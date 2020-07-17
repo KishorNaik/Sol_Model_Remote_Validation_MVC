9c66d42e9ba8727b374c3aaba59121c6951209c5
Kishor Naik <kishor.naik011.net@outlook.com>
2020-07-17 14:03:33 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sol_Demo.Models;
+
+namespace Sol_Demo.Controllers
+{
+    public class UsersController : Controller
+    {
+        [BindProperty]
+        public UserModel Users { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("onsubmit", Name = "onsubmit")]
+        public IActionResult Submit()
+        {
+            if (base.ModelState.IsValid)
+            {
+                return Content("Validation Pass");
+            }
+            else
+            {
+                return View("Index", Users);
+            }
+        }
+
+        // Remote Validation (Is Email Id exists or not in server)
+        [HttpGet]
+        public IActionResult IsEmailExistRemoteValidation(string emailId)
+        {
+            if (emailId == "kishor.naik011.net@gmail.com")
+            {
+                return Json(false);
+            }
+            else
+            {
+                return Json(true);
+            }
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Models/UserModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Models
+{
+    public class UserModel
+    {
+        // https://docs.microsoft.com/en-us/aspnet/core/mvc/models/validation?view=aspnetcore-3.1
+        // Pre Defined Data Annotations
+        [Required]
+        [MaxLength(50, ErrorMessage = "First Name should be less than 50 character")]
+        [DisplayName("First Name")]
+        public String FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "First Name should be less than 50 character")]
+        [DisplayName("First Name")]
+        public String LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Remote("IsEmailExistRemoteValidation", "Users", ErrorMessage = "The given email id is already existed")]
+        [DisplayName("Email Id")]
+        public String EmailId { get; set; }
+    }
+}
